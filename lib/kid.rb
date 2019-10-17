77df4b3a9d87e053dc24f09037a0c9946150f29d
generous-vector-9581 <ojappah@gmail.com>
2019-10-17 20:14:17 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,8 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 class Kid
   extend MetaDancing
-
+extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   attr_accessor :name
 
   def initialize(name)
