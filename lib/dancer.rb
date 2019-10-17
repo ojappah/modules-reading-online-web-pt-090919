fd2213caae180e2116423b7f5e294d14be491984
generous-vector-9581 <ojappah@gmail.com>
2019-10-17 19:54:03 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,12 +1,10 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
-  include Dance
+  extend MetaDancing
  
   attr_accessor :name
- class Dancer
-  extend MetaDancing
-end
+
   def initialize(name)
     @name = name
   end
